1044030a625e41123be033ca71ba8e1880aa7bc4
caaarl-aaa <celiareyes05@outlook.com>
2025-03-05 14:29:24 +0800

    Update first_page.py

--- first_page.py
+++ first_page.py
@@ -1,55 +1,39 @@
 import customtkinter as ctk
 import random
-from PIL import Image
+from PIL import Image, ImageTk
 import second_page_login
 
 def open_second_page():
-    second_window=second_page_login.run_login_page(app)
-    if second_window:
-        app.withdraw()
-        second_window.wait_window()
-        app.deiconify
+    for widget in app.winfo_children():
+        widget.destroy()  # Remove current widgets
+    second_page_login.create_login_page(app)
     
 
 #Intilialize App
 app = ctk.CTk()
 app.title("Physiotherapy App")
-
-
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
-
 app.geometry(f"{screen_width}x{screen_height}+0+0")
 
 
 # Create Canvas for Background Pattern
 canvas = ctk.CTkCanvas(app, width=screen_width, height=screen_height, bg= "#89B4B7", highlightthickness=0)
 canvas.place(relwidth=1, relheight=1)
+screen_width+=350
 
 # Generate Random Circles for Background
-positions = [(100, 30),(400,90),(300,screen_height-170),(624,233),(-30,300),(screen_width-30,100),(screen_width-750,70),(screen_width-423,235),(screen_width-320,623),(screen_width-50,screen_height-50)]
+positions = [(150, 30),(450,90),(350,screen_height-170),(674,233),(-30,300),(screen_width-80,100),(screen_width-700,70),(screen_width-400,235),(screen_width-300,623),(screen_width-50,screen_height-50)]
 for pos in positions:
-    size = random.randint(40, 100)  # Adjust for better visibility
+    size = random.randint(60, 150)  # Adjust for better visibility
     canvas.create_oval(pos[0], pos[1], pos[0] + size, pos[1] + size, fill="#789FA1", outline="")
 
-# UI Elements (Centered)
-logo_image = ctk.CTkImage(light_image=Image.open("assets_gui/logo.png"),
-                          dark_image=Image.open("assets_gui/logo.png"),
-                          size=(300, 300))  # Adjust size if needed
-
-# Create a label for the logo
-logo_label = ctk.CTkLabel(app, image=logo_image, text="", bg_color="#89B4B7")  # No text, only image
-logo_label.place(relx=0.5, rely=0.3, anchor="center")
-
-
-alt_font=ctk.CTkFont(family="Garamond", size=55, weight='bold')
-title2 = ctk.CTkLabel(app, text="An Intelligent Physiotherapy and Recovery App\nfor Personalized and Remote Care",
-                      font=alt_font, text_color="black",bg_color= "#89B4B7",justify="center")
-title2.place(relx=0.5, rely=0.55, anchor="center")
-
-subtitle = ctk.CTkLabel(app, text="Welcome to our Medical Application",
-                        font=("Georgia", 25), text_color="black", bg_color= "#89B4B7")
-subtitle.place(relx=0.5, rely=0.66, anchor="center")
+prof_image = Image.open("assets_gui/logo.png").resize((400, 400), Image.LANCZOS)
+prof_photo = ImageTk.PhotoImage(prof_image)
+canvas.create_image(950,250,image=prof_photo)
+canvas.profimage = prof_photo
+canvas.create_text(950,550,text="An Intelligent Physiotherapy and Recovery App\n           for Personalized and Remote Care",fill="#000000",font=("Garamond", 60, 'bold'))
+canvas.create_text(700,650,anchor="nw",text="Welcome to our Medical Application",fill="#000000",font=("Garamond", 30))
 
 # Get Started Button
 start_button = ctk.CTkButton(app, text="Get Started", font=("Arial", 23, "bold"), fg_color="#275057", bg_color="#89B4B7",
@@ -58,8 +42,6 @@ start_button.place(relx=0.5, rely=0.74, anchor="center")
 def exit_app(root):
     root.quit()  # Stop the mainloop
     root.destroy()  # Close all windows
-
-app.protocol("WM_DELETE_WINDOW", lambda: exit_app(app))
 
 # Run App
 app.mainloop()
